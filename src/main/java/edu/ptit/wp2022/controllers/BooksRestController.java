70733f5f8a34f3ba43665c3bf848b1ef4845e89e
THINKPADT580\TQH <tranhung99nt@gmail.com>
2022-10-30 21:26:17 +0700

    init

---
+++ src/main/java/edu/ptit/wp2022/controllers/BooksRestController.java
@@ -0,0 +1,8 @@
+package edu.ptit.wp2022.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BooksRestController {
+}
---
+++ src/test/java/edu/ptit/wp2022/WebProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ptit.wp2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebProgrammingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
